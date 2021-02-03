3f20a94160ce732994062ebc37ef5e8510f70d5b
tkp-y <tfk15@georgetown.edu>
2021-02-03 17:40:13 -0500

    Ask user for input

--- game.py
+++ game.py
@@ -1,2 +1,34 @@
 
 print("Rock, Paper, Scissors, Shoot!")
+
+
+print("-------------------")
+print("Welcome 'Player One' to my Rock-Paper-Scissors game...")
+print("-------------------")
+
+#asking user for an input
+
+
+x = input("Please choose either 'rock', 'paper', or 'scissors': ")
+
+print(x)
+
+
+#string interpolation / format string usage
+#print(f"You chose: {x}")
+
+
+print("You chose: " + x)
+
+exit()
+
+#simulating a computer input
+
+print("The computer chose: 'paper'")
+
+#determining who won
+
+print("-------------------")
+print("Oh, the computer won. It's ok.")
+print("-------------------")
+print("Thanks for playing. Please play again!")
